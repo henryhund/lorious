e91e2fe9e0a152784ea0deffa71b3f46963122aa
kevivmatrix <vivekpatna52@gmail.com>
2013-09-25 12:16:49 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lorious::Application.config.secret_key_base = '3c460d7a3eeee5a2dfa1ca9fb5e50be9933b9624a520e46f46dcf48e44b87a462432906f93704a91fb28abb5429669c8e9d79d1a7972915ed8ec518f4861afbf'
