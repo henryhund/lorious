3e11ff7650d03ab9a83eea6d8e4f2bea1048ec9b
kevivmatrix <vivekpatna52@gmail.com>
2013-10-08 14:39:31 +0530

    profile page with dummy content

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Lorious::Application.routes.draw do
   namespace :users do
     resources :social_media
     resources :steps, only: [:edit, :update]
+    resource :profile, only: [:show]
   end
 
-  match "profile" => "users#show", as: :profile, via: [:get]
   resource :users, only: [:show]
   resources :invites
   
   root :to => "home#index"
   get '/search', to: 'home#search'
   get "/search/page/:page", :controller => "home", :action => "search"
+
+  match "/:username" => "users/profiles#show", via: [:get], as: :profile
 end
