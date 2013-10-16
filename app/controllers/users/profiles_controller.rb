7a14e91ffbe6a3562a6aa9554d56a0a2b2cc8f7a
kevivmatrix <vivekpatna52@gmail.com>
2013-10-16 18:03:34 +0530

    load expertise for expert only

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -5,7 +5,7 @@ class Users::ProfilesController < ApplicationController
     if @user
       @social_links = @user.social_links
       @interests = @user.interests
-      @expertise = @user.expertise
+      @expertise = @user.expertise if @user.expert?
       @reviews = @user.reviews_received.includes(:reviewer, :tags)
       render :show
     else
