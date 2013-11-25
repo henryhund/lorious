d3a244dfe1e384dbbb582872cbacb437ff3fe9a7
pdhar <pranav.dhar2@gmail.com>
2013-11-25 15:47:46 +0530

    assets compiled

--- app/controllers/users/appointments_controller.rb
+++ app/controllers/users/appointments_controller.rb
@@ -7,15 +7,15 @@ class Users::AppointmentsController < ApplicationController
   end
 
   def pending
-    @appointments = Appointment.pending.where("user_id = ? OR expert_id = ?", current_user.id, current_user.id)
+    @appointments = Appointment.pending.where("user_id = ? OR expert_id = ?", current_user.id, current_user.id).order(:created_at => :desc)
   end
 
   def upcoming
-    @appointments = Appointment.upcoming.where("user_id = ? OR expert_id = ?", current_user.id, current_user.id)
+    @appointments = Appointment.upcoming.where("user_id = ? OR expert_id = ?", current_user.id, current_user.id).order(:created_at => :desc)
   end
 
   def history
-    @appointments = Appointment.history.where("user_id = ? OR expert_id = ?", current_user.id, current_user.id)
+    @appointments = Appointment.history.where("user_id = ? OR expert_id = ?", current_user.id, current_user.id).order(:created_at => :desc)
   end
 
 end
--- app/controllers/users/credits_controller.rb
+++ app/controllers/users/credits_controller.rb
@@ -2,7 +2,7 @@ class Users::CreditsController < ApplicationController
   layout false
   
   def transactions
-    @transactions = current_user.transactions_made
+    @transactions = current_user.transactions_made.order(:created_at => :desc)
   end
   
 end
