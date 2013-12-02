132b7ab3fed8ed51ef992576aff35f1ff24ce6c8
pdhar <pranav.dhar2@gmail.com>
2013-12-02 12:11:14 +0530

    hangout fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_no_cache, :profile_not_completed
-   
+   before_filter :current_user, :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   rescue_from CanCan::AccessDenied do |exception|
    redirect_to '/', :alert => exception.message
@@ -27,6 +28,28 @@ class ApplicationController < ActionController::Base
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
   
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = '*'
+  headers['Access-Control-Request-Method'] = '*'
+  headers['Access-Control-Max-Age'] = "1728000"
+end
+
+# If this is a preflight OPTIONS request, then short-circuit the
+# request, return only the necessary headers and return an empty
+# text/plain.
+
+def cors_preflight_check
+  if request.method == :options
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :content_type => 'text/plain'
+  end
+end
+
   def profile_not_completed
     @user = current_user
     if @user.present?
