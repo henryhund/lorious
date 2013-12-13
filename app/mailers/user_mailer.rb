4b41c0264dc37332912f3cb87f05012f5c67676c
Henry Hund <henryhund@gmail.com>
2013-12-13 13:00:58 -0500

    updated usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@lorious.com"
   layout 'mail_layout'
-   
+
   @@base_url = "http://" + Rails.configuration.action_mailer.default_url_options[:host]
 
   def invite_approved invite
@@ -14,23 +14,31 @@ class UserMailer < ActionMailer::Base
     @expert = expert
     mail(to: @expert.email, subject: 'Expert Application Aprroved')
   end
-  
+
   def merchant_account_approved expert
     @expert = expert
     mail(to: @expert.email, subject: 'Merchant Account Aprroved')
   end
-  
+
   def merchant_account_declined expert, reason
     @expert, @reason = expert, reason
     mail(to: @expert.email, subject: 'Merchant Account Declined')
   end
-    
+
   def test_mail
     @heading = " Appointment Completed. "
     mail(to: "pranav.dhar2@gmail.com",
          subject: "Test Mail")
   end
-  
+
+  def some_mail
+    @head = "This is the heading"
+    mail(to: @support_mail.nil? ? "support@lorious.com" : @support_mail,
+         body: " has applied to be an Expert!",
+         content_type: "text/html",
+         subject: " has applied to be an Expert!")
+  end
+
   def expert_applied expert
     @support_mail = Setting.find_by(name: "support_email_id").value
     mail(to: @support_mail.nil? ? "support@lorious.com" : @support_mail,
@@ -38,7 +46,7 @@ class UserMailer < ActionMailer::Base
          content_type: "text/html",
          subject: expert + " has applied to be an Expert!")
   end
-  
+
   def after_appointment_cancel appointment, reason
     @support_mail = Setting.find_by(name: "support_email_id").value
     mail(to: @support_mail.nil? ? "support@lorious.com" : @support_mail,
@@ -47,35 +55,35 @@ class UserMailer < ActionMailer::Base
          content_type: "text/html",
          subject: appointment.user.name + " has requested cancellation")
   end
-  
+
   def request_created_suggest_experts requests, to
      @requests, @to = requests, to
      mail(to: @to.email, subject: 'Recommended Requests')
   end
-  
+
   def new_appointment_request appointment, from, to
     @appointment, @from, @to, @expert = appointment, from, to, appointment.expert
     mail(to: @to.email, subject: 'New Appointment')
   end
-  
+
   def appointment_reminder appointment, to
     @appointment, @to, @appointment_with = appointment, to, appointment.appointment_with_for_user(to)
     @heading = "Upcoming Appointment!"
-    mail(to: @to.email, subject: 'You have an appointment coming up in about an hour!')  
+    mail(to: @to.email, subject: 'You have an appointment coming up in about an hour!')
   end
-  
+
   def appointment_completed appointment, to
     @appointment, @to, @appointment_with = appointment, to, appointment.appointment_with_for_user(to)
     @heading = "Appointment Completed."
-    mail(to: @to.email, subject: 'Appointment Completed')  
+    mail(to: @to.email, subject: 'Appointment Completed')
   end
-  
+
   def appointment_cancelled appointment, to, edited_by
     @appointment, @to, @edited_by, @appointment_with = appointment, to, edited_by, appointment.appointment_with_for_user(to)
     @heading = "Appointment Cancelled."
     mail(to: @to.email, subject: 'Appointment Cancelled')
   end
-  
+
   def appointment_updated_confirm_request appointment, to, edited_by
     @appointment, @to, @edited_by, @appointment_with = appointment, to, edited_by, appointment.appointment_with_for_user(to)
     @heading = "Confirm Your Appointment."
@@ -87,7 +95,7 @@ class UserMailer < ActionMailer::Base
     @heading = "Appointment Confirmed."
     mail(to: @to.email, subject: 'Appointment Confirmed')
   end
-  
+
   def payment_successful_notification to, result
     @to, @result = to, result
     @heading = "Payment Confirmed."
