c2a3013e99011891389dc5b1090db8cb1f3a4ada
Henry Hund <henryhund@gmail.com>
2014-01-18 13:32:25 -0500

    updated braintree initializer

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "kpybphyjj5jn8j5q"
-Braintree::Configuration.public_key = "v4qb6wkgd68ym5x2"
-Braintree::Configuration.private_key = "6d0d9980e7178b39f69c418939fd2faf"
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
