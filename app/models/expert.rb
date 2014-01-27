01f1aa7199ad2e846b95bbc023e139a419428118
Henry Hund <henryhund@gmail.com>
2014-01-27 13:12:56 -0500

    fix get_average_rating function

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -3,29 +3,29 @@ class Expert < User
   acts_as_taggable_on :subscriptions
 
   CASH_TO_CREDIT_RATIO = 1.0
-  
+
   has_one :availability
   has_many :appointments, foreign_key: "expert_id"
   has_many :expertise
-  
+
   searchable :auto_index => true, :auto_remove => true do
     text :first_name , :as => :first_name_textp
     text :last_name , :as => :last_name_textp
     text :bio , :as => :bio_textp
-    
+
     text :skill_list
-    string :skill_list, :multiple => true, :stored => true do 
+    string :skill_list, :multiple => true, :stored => true do
       skill_list.map!{|c| c.downcase.strip}
     end
-    latlon(:location) { 
+    latlon(:location) {
        Sunspot::Util::Coordinates.new(self.latitude, self.longitude)
      }
   end
-  
+
   def attributes
     super.merge({'skill_list' => skill_list, 'hourly_rate' => hourly_rate, 'average_rating' => average_rating , 'name' => name})
   end
-  
+
   def set_availability(availability, timezone_in_minutes, availability_unit_in_minutes=30)
     availability_object = self.availability || self.build_availability
     availability_object.set_availability_in_gmt(availability, timezone_in_minutes, availability_unit_in_minutes)
@@ -35,13 +35,13 @@ class Expert < User
   def get_availability(availability, timezone_in_minutes)
     self.availability.is_free?(availability, timezone_in_minutes)
   end
-  
+
   def get_hourly_rate()
     self.availability.try(:hourly_cost) || 0.00
   end
-  
+
   def get_average_rating()
-    Review.where(:reviewer_id => self.id).average('rating').round rescue 0
+    Review.where(:reviewed_id => self.id).average('rating').round rescue 0
   end
 
   def hourly_rate_in_credit
@@ -55,21 +55,21 @@ class Expert < User
       false
     end
   end
-  
+
   def mark_appointment_reviewed(user_id)
     apt = self.appointments.find_by(user_id: user_id, is_reviewed: false)
     apt.is_reviewed = true
     apt.save validate:false
   end
-  
+
   alias_method :average_rating, :get_average_rating
   alias_method :hourly_rate, :get_hourly_rate
   before_save :set_default_subscriptions
-  
+
   private
     def set_default_subscriptions
       if self.subscription_list.size == 0
-        self.subscription_list.add self.skill_list  
+        self.subscription_list.add self.skill_list
       end
     end
 end
