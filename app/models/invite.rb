73bed514ee07846b7dc511d95391d365a151ee25
Henry Hund <henryhund@gmail.com>
2014-01-27 12:55:21 -0500

    fixed apostrophe issue

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,8 +1,8 @@
 class Invite < ActiveRecord::Base
 
   validates :name, :email, presence: true
-  validates_format_of :name, 
-          :with => /\A[a-zA-Z\s]+\z/ix,
+  validates_format_of :name,
+          :with => /\A[a-zA-Z\'\s]+\z/ix,
           :message => "only letters allowed"
   validates_format_of :email,
           :with => /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/ix,
